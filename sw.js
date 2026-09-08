0310deb920a232dafb9bf738ba863e9883c13a2d
sreedward <admieemp@gmail.com>
2026-09-08 10:31:39 -0500

    fix sw: auto-reload pages on SW update, bump v9
    Updated CACHE_VERSION from 'v8' to 'v9' and modified the activate event to refresh all clients.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v8'
+const CACHE_VERSION = 'v9'
 const STATIC_CACHE = `cqm-static-${CACHE_VERSION}`
 const API_CACHE    = `cqm-api-${CACHE_VERSION}`
 const STATIC_ASSETS = [
@@ -20,9 +20,14 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== STATIC_CACHE && k !== API_CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    ).then(async () => {
+      await self.clients.claim()
+      const allClients = await self.clients.matchAll({ type: 'window' })
+      allClients.forEach(c => { try { c.navigate(c.url) } catch(err) {} })
+    })
   )
 })
+
 
 self.addEventListener('fetch', event => {
   const req = event.request
