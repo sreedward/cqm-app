a1373022c5be48ca9acff655813671335d58b273
sreedward <admieemp@gmail.com>
2026-09-10 15:27:54 -0500

    Update cache version from v11 to v12Bump SW cache to v12 to force old SW out on PC

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v11'
+const CACHE_VERSION = 'v12'
 const STATIC_CACHE = `cqm-static-${CACHE_VERSION}`
 const API_CACHE    = `cqm-api-${CACHE_VERSION}`
 const STATIC_ASSETS = [
