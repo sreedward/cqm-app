3595dab796726c5e2808ef8b7a0ba2a797c58ed9
sreedward <admieemp@gmail.com>
2026-09-10 21:08:54 -0500

    Fix medley card navigation across devices

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v12'
+const CACHE_VERSION = 'v13'
 const STATIC_CACHE = `cqm-static-${CACHE_VERSION}`
 const API_CACHE    = `cqm-api-${CACHE_VERSION}`
 const STATIC_ASSETS = [
