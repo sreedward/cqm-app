c457e11ff8411d7f345b33b9f989c60679241d81
sreedward <admieemp@gmail.com>
2026-09-10 21:41:57 -0500

    Add lead-controlled medley chord editing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v14'
+const CACHE_VERSION = 'v15'
 const STATIC_CACHE = `cqm-static-${CACHE_VERSION}`
 const API_CACHE    = `cqm-api-${CACHE_VERSION}`
 const STATIC_ASSETS = [
